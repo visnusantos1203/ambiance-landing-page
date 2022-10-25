5c61a93acad9baa9dc1bbca47326eed90e14f833
Dash <visnu.santos1203@gmail.com>
2022-10-25 22:31:04 +0800

    Tailwind Setup

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        './public/index.html',
+        "./src/**/*.{html,js}"
+    ],
+    theme: {
+      container: {
+        center: true,
+        padding: '2rem'
+      },
+    },
+    plugins: [],
+  }
